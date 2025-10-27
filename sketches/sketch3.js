fe8624a7ef975873536ac64e4a1b07a80c435409
Karl <k.lee131530@gmail.com>
2025-10-26 20:01:22 -0700

    Added rim and lid button of rice cooker

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -10,6 +10,12 @@ registerSketch('sk3', function (p) {
     p.noStroke();
     p.fill(245);
     p.ellipse(p.width / 2, p.height / 2 + 50, 500, 450);
+
+    p.fill(200, 160, 160);
+    p.ellipse(p.width / 2, p.height / 2 - 165 + 50, 365, 130);
+
+    p.fill(220, 180, 180);
+    p.ellipse(p.width / 2, p.height / 2 - 195 + 50, 60, 30);
   };
 
   p.windowResized = function () {
