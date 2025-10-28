236f1ccc586e62cd53c47f33e2b72e16dcb3d6a8
Karl <k.lee131530@gmail.com>
2025-10-27 22:33:26 -0700

    Added burners that correspond to time values with color intensity

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -6,6 +6,46 @@ registerSketch('sk4', function (p) {
   
   p.draw = function () {
     p.background(30, 30, 32);
+
+    drawBurner(180, 220, 140, p.hour() % 12, 12);
+    drawBurner(520, 220, 80, p.second(), 60);
+    drawBurner(350, 380, 110, p.minute(), 60);
   };
+
+  function drawBurner(x, y, size, value, maxValue) {
+    let intensity = value / maxValue;
+
+    let burnerColor = p.lerpColor(
+      p.color(20, 20, 22),
+      p.color(255, 220, 80),
+      p.pow(intensity, 0.7)
+    );
+
+    if (intensity > 0.3) {
+      burnerColor = p.lerpColor(
+        p.color(180, 40, 10),
+        p.color(255, 220, 80),
+        (intensity - 0.3) / 0.7
+      );
+    }
+
+    if (intensity > 0.1) {
+      p.noStroke();
+      p.fill(255, 100, 30, intensity * 120);
+      p.ellipse(x, y, size + 50, size + 50);
+
+      if (intensity > 0.5) {
+        p.fill(255, 200, 100, intensity * 100);
+        p.ellipse(x, y, size + 30, size + 30);
+      }
+    }
+    
+    p.fill(burnerColor);
+    p.noStroke();
+    p.ellipse(x, y, size, size);
+
+    p.fill(0, 0, 0, 40);
+    p.ellipse(x, y, size * 0.3, size * 0.3);
+  }
 });    
     
