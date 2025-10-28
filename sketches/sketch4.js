148dbfd000fe31a6291b2a2e19c9a3874f3708f7
Karl <k.lee131530@gmail.com>
2025-10-27 22:26:46 -0700

    Added canvas setup and stovetop background

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,14 +1,11 @@
 // Instance-mode sketch for tab 4
 registerSketch('sk4', function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(700, 600);
   };
+  
   p.draw = function () {
-    p.background(200, 240, 200);
-    p.fill(30, 120, 40);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. C', p.width / 2, p.height / 2);
+    p.background(30, 30, 32);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
-});
+});    
+    
